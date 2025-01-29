69cfcbb9786510228c945ce8858421b45855fb53
MichaelJG.Kopp <michaeljg.kopp@gmail.com>
2025-01-29 21:50:21 +0100

    show error for login form

--- src/main/java/com/luv2code/springdemo/mvc/controller/CustomerController.java
+++ src/main/java/com/luv2code/springdemo/mvc/controller/CustomerController.java
@@ -1,12 +1,13 @@
 package com.luv2code.springdemo.mvc.controller;
 
 import com.luv2code.springdemo.mvc.model.Customer;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 //@RequestMapping("/customer")
@@ -21,10 +22,16 @@ public class CustomerController {
     }
 
     @PostMapping("/processForm")
-    public String processForm(@ModelAttribute Customer customer) {
+    public String processForm(
+            @Valid @ModelAttribute Customer customer,
+            BindingResult bindingResult) {
 
         System.out.println("Customer: " + customer.getFirstName() + " " + customer.getLastName());
 
-        return "customer-confirmation";
+        if (bindingResult.hasErrors()) {
+            return "customer-form";
+        } else {
+            return "customer-confirmation";
+        }
     }
 }
