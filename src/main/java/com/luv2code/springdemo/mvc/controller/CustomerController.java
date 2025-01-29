442c9d68a6f61c8da6f0473d4f5ae3d127100357
MichaelJG.Kopp <michaeljg.kopp@gmail.com>
2025-01-29 21:53:46 +0100

    add initBinder with customEditor to remove whitespaces from input

--- src/main/java/com/luv2code/springdemo/mvc/controller/CustomerController.java
+++ src/main/java/com/luv2code/springdemo/mvc/controller/CustomerController.java
@@ -2,16 +2,27 @@ package com.luv2code.springdemo.mvc.controller;
 
 import com.luv2code.springdemo.mvc.model.Customer;
 import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 //@RequestMapping("/customer")
 public class CustomerController {
+
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder) {
+
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true); // true: trim to null if all whitespace
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+
+    }
 
     @GetMapping("/")
     public String showForm(Model model) {
