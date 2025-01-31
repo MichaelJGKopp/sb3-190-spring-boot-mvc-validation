e09d9ea6b75a1e70082fba0bdaf16f1af94052aa
MichaelJG.Kopp <michaeljg.kopp@gmail.com>
2025-01-31 19:27:47 +0100

    change annotation value

--- src/main/java/com/luv2code/springdemo/mvc/model/Customer.java
+++ src/main/java/com/luv2code/springdemo/mvc/model/Customer.java
@@ -23,7 +23,7 @@ public class Customer {
     @Pattern(regexp="^[a-zA-Z0-9]{5}", message="only 5 chars/digits")
     private String postalCode;
 
-    @CourseCode(value="LUV", message="must start with LUV")
+    @CourseCode(value="TOPS", message="must start with TOPS")
     private String courseCode;
 
     public Customer() {
